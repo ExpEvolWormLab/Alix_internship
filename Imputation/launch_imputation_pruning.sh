32793cec05b7db64cae922f30cff9c985ea3387e
Xillanne <83120878+Xillanne@users.noreply.github.com>
2024-06-24 13:40:45 +0200

    Update launch_imputation_pruning.sh

--- Imputation/launch_imputation_pruning.sh
+++ Imputation/launch_imputation_pruning.sh
@@ -1,6 +1,8 @@
 #!bin/bash
-
 ## Script which launch imputation by populations, filter it and prune it 
+path_gatk='/mnt/data2/desmarais/gatk-4.5.0.0/gatk'
+
+
 # First feature name of the vcf file
 # Second feature name of the output directory
 
@@ -12,7 +14,7 @@ bash script_imputation.sh $1 $2
 for pop in A6 CA EEV GA GM GT LR SMR # For each population
 do
 	bcftools concat imputed_*_$pop.vcf.gz -O z > imputed_All_$pop.vcf.gz
-	bash script_filtration_imputation.sh imputed_All_$pop.vcf.gz $pop
+	bash script_filtration_imputation.sh imputed_All_$pop.vcf.gz $pop $path_gatk
 done &
 
 wait
