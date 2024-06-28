267b5809638a04cfab2394f5982cff90f01fc3b5
Xillanne <83120878+Xillanne@users.noreply.github.com>
2024-06-28 15:09:51 +0200

    Update G-matrix.R

--- G-matrix/G-matrix.R
+++ G-matrix/G-matrix.R
@@ -1,17 +1,12 @@
-## Script to compute G-matrix using MCMCglmm 
-# Same as FM script
+## Script to compute G-matrix using MCMCglmm
 
-# To change
+# Get command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
-M_file <- args[1] 
-condition <- args[2]
-output <- args[3]
+M_file <- args[1]  # Path to the M file
+condition <- args[2]  # Condition to filter phenotype data
+output <- args[3]  # Output directory
 
-M_file <- 'A6_t_convert_genotype.csv'
-setwd("~/Documents/Worms/GBLUP")
-condition <- 'NGM'
-
-# Load packages
+# Load necessary packages
 options(rgl.useNULL=TRUE)
 library(data.table)
 library(lattice)
@@ -21,61 +16,51 @@ library(reshape2)
 library(gridExtra)
 library(matrixStats)
 
+# Load and process the M matrix
 M <- as.data.frame(fread(M_file))
-rownames(M) <- M[,1]
-M[,1] <- NULL
-M <- as.matrix(M)
+rownames(M) <- M[, 1]  # Set row names
+M[, 1] <- NULL  # Remove the first column
+M <- as.matrix(M)  # Convert to matrix
 
-
-# Load pheno data
+# Load phenotype data
 pheno <- read.csv('Final_Transition_rates_estimates_may2024_export.csv')
-pheno_subset <- pheno[,c('pop_label','temperature','rel_humidity',"session_id",
-                  'logD','date_str','env_label',
-                  "SF",
-                  "SB",
-                  "FS",
-                  "FB",
-                  "BS",
-                  "BF")]
+pheno_subset <- pheno[, c('pop_label', 'temperature', 'rel_humidity', "session_id",
+                          'logD', 'date_str', 'env_label', "SF", "SB", "FS", "FB", "BS", "BF")]
 
-## Kept only lines with phenotypes
-pheno_subset<-pheno_subset[pheno_subset$env_label==condition,]
-M <- M[rownames(M) %in% pheno_subset$pop_label,]
+# Keep only lines with phenotypes for the specified condition
+pheno_subset <- pheno_subset[pheno_subset$env_label == condition, ]
+M <- M[rownames(M) %in% pheno_subset$pop_label, ]
 
-
-# Scale covariates and reduce traits
-vect_P_traits <- c("SF",
-                   "SB",
-                   "FS",
-                   "FB",
-                   "BS",
-                   "BF")
+# Scale covariates and standardize traits
+vect_P_traits <- c("SF", "SB", "FS", "FB", "BS", "BF")
 
 pheno_subset$temperature <- as.numeric(pheno_subset$temperature)
 pheno_subset$rel_humidity <- as.numeric(pheno_subset$rel_humidity)
 pheno_subset$logD <- as.numeric(pheno_subset$logD)
-for(j in c('temperature',"rel_humidity","logD")){
-  pheno_subset[,j][is.na(pheno_subset[,j])] <- mean(pheno_subset[,j], na.rm = TRUE)
-  pheno_subset[,j] <- (pheno_subset[,j]-mean(pheno_subset[,j], na.rm = TRUE))/sd(pheno_subset[,j], na.rm = TRUE)
-}
-for(j in vect_P_traits){
-  pheno_subset[,j]<-(pheno_subset[,j]-mean(pheno_subset[,j], na.rm = TRUE))
+
+# Standardize covariates
+for (j in c('temperature', "rel_humidity", "logD")) {
+  pheno_subset[, j][is.na(pheno_subset[, j])] <- mean(pheno_subset[, j], na.rm = TRUE)
+  pheno_subset[, j] <- (pheno_subset[, j] - mean(pheno_subset[, j], na.rm = TRUE)) / sd(pheno_subset[, j], na.rm = TRUE)
 }
 
-
+# Center traits
+for (j in vect_P_traits) {
+  pheno_subset[, j] <- (pheno_subset[, j] - mean(pheno_subset[, j], na.rm = TRUE))
+}
 
 #### G-matrix
 
 # Define prior model for GBLUP
 nb_trait = length(vect_P_traits)
 phen.var = diag(nb_trait) * diag(var(subset(pheno_subset, select = vect_P_traits)))
-prior_mod <- list(G = list(G1 = list(V = phen.var/3, nu = nb_trait),
-                           G2 = list(V = phen.var/3, nu = nb_trait)), 
-                  R = list(V = phen.var/3, nu = nb_trait))
-# MCMCglmm
+prior_mod <- list(G = list(G1 = list(V = phen.var / 3, nu = nb_trait),
+                           G2 = list(V = phen.var / 3, nu = nb_trait)), 
+                  R = list(V = phen.var / 3, nu = nb_trait))
 
+# Fit the MCMCglmm model
 if (length(unique(pheno_subset$session_id)) != 1) {
-  model_MCMC_WI <- MCMCglmm(cbind(c(SF, SB, FS, FB, BS, BF)) ~ (logD + rel_humidity + temperature + session_id)^4 + trait - 1, 
+  model_MCMC_WI <- MCMCglmm(cbind(SF, SB, FS, FB, BS, BF) ~ (logD + rel_humidity + temperature + session_id)^4 + trait - 1, 
                             random = ~us(trait):pop_label + us(trait):date_str, 
                             rcov = ~us(trait):units, 
                             family = rep("gaussian", nb_trait), 
@@ -85,7 +70,7 @@ if (length(unique(pheno_subset$session_id)) != 1) {
                             burnin = 10000, 
                             thin = 100)
 } else {
-  model_MCMC_WI <- MCMCglmm(cbind(c(SF, SB, FS, FB, BS, BF)) ~ (logD + rel_humidity + temperature)^4 + trait - 1, 
+  model_MCMC_WI <- MCMCglmm(cbind(SF, SB, FS, FB, BS, BF) ~ (logD + rel_humidity + temperature)^4 + trait - 1, 
                             random = ~us(trait):pop_label + us(trait):date_str, 
                             rcov = ~us(trait):units, 
                             family = rep("gaussian", nb_trait), 
@@ -96,24 +81,14 @@ if (length(unique(pheno_subset$session_id)) != 1) {
                             thin = 100)
 }
 
-
-
-# # Save model results
+# Save model results
 write.csv(model_MCMC_WI$Sol, gsub('XXX', output, 'XXX_MCMCmodel_Sol.csv'), quote = FALSE, row.names = FALSE)
 write.csv(model_MCMC_WI$VCV, gsub('XXX', output, 'XXX_MCMCmodel_VCV.csv'), quote = FALSE, row.names = FALSE)
-write.csv( posterior.mode(model_MCMC_WI$VCV), gsub('XXX', output, 'XXX_MCMCmodel_VCV_posterior.mode.csv'), quote = FALSE, row.names = FALSE)
 
 # Print model summary
 print(summary(model_MCMC_WI))
 
+# Compute and save the G-matrix
 post_dist = posterior.mode(model_MCMC_WI$VCV)
-WI_G_mat=matrix(post_dist[1:nb_trait^2],nb_trait, nb_trait)
-write.csv( WI_G_mat, gsub('XXX', output, 'XXX_MCMCmodel_VCV_G_matrix.csv'), quote = FALSE, row.names = FALSE)
-
-
-# # Exit with a status code
-# if (length(unique(pheno_subset$session_id)) != 1) {
-#   quit(status = 0)
-# } else {
-#   quit(status = 1)
-# }
+WI_G_mat = matrix(post_dist[1:nb_trait^2], nb_trait, nb_trait)
+write.csv(WI_G_mat, gsub('XXX', output, 'XXX_MCMCmodel_VCV_G_matrix.csv'), quote = FALSE, row.names = FALSE)
