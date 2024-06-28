e03d3f958dc53792d7aad10e056a78a065036722
Xillanne <83120878+Xillanne@users.noreply.github.com>
2024-06-28 13:34:53 +0200

    Update Get_SNP_position.R

--- Get_SNP_position.R
+++ Get_SNP_position.R
@@ -1,29 +1,30 @@
-## Script to get the position of SNPs present in a set of vcf file with similar names
+## Script to get the position of SNPs present in a set of VCF files with similar names
 
+# Set working directory
 setwd("~/Documents/Worms/Plot_GATK/Pop")
 
+# Define the pattern to match VCF files
 motif <- '(.*)_final.vcf.gz'
-# Charger les fichiers vcf
-files <- list.files(pattern = motif )
 
+# Load necessary library
 library(vcfR)
 
+# List all files in the directory matching the pattern
+files <- list.files(pattern = motif)
+
+# Read each VCF file into a list of vcfR objects
 dfs <- lapply(files, read.vcfR)
-files_name <- sub(motif ,"\\1",files)
 
+# Extract the base names of the files (without the suffix)
+files_name <- sub(motif, "\\1", files)
 
-
-##### First step : kinship matrix ######
-
-for(i in dfs){
-  vcf<-dfs[[i]]
+# Loop through each VCF object and extract SNP positions
+for(i in seq_along(dfs)){
+  vcf <- dfs[[i]]  # Get the i-th vcfR object
   
-  # Extract genotype information
-  genotype_info <- vcf@gt
-  
-  # Extract SNP positions
+  # Extract SNP positions (CHROM and POS columns from the fixed section of the VCF)
   snp_positions <- vcf@fix[, c("CHROM", "POS")]
   
-  write.csv(snp_positions,paste0(files_name,'_snp_positions.csv'),row.names = FALSE,quote = FALSE)
+  # Write the SNP positions to a CSV file
+  write.csv(snp_positions, paste0(files_name[i], '_snp_positions.csv'), row.names = FALSE, quote = FALSE)
 }
-
