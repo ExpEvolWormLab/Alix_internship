5e7a9ec3d8c0e4cf1c7c50d2dca657a2f7ae3159
Xillanne <83120878+Xillanne@users.noreply.github.com>
2024-06-28 13:32:06 +0200

    Update Summarize_SNPsEffect_pruned.R

--- Plots/Summarize_SNPsEffect_pruned.R
+++ Plots/Summarize_SNPsEffect_pruned.R
@@ -1,41 +1,53 @@
-### Script to summarize SNPs effects for pruned files
+### Script to summarize SNPs effects for pruned file (specific for them, because there is mistake in lines'name, they are duplicated Name_Name)
+## Input : 1. SNPs_effects*csv - output of Pipeline_GBLUP
+##         2. Name of output
+## Output : Summary*csv - Name of SNP (CHROM_POS), CHROM, Median (of SNPs effect a posteriori distribution), lower (of credibility interval), upper, Credibility
+
+# Load necessary libraries
+library(HDInterval)  # For computing highest density intervals
+library(data.table)  # For fast data reading and manipulation
+library(ggplot2)  # For plotting
+library(matrixStats)  # For efficient matrix operations
+
+# Get command line arguments
 args <- commandArgs(trailingOnly = TRUE)
-file <-  args[1] 
-output <- args[2]
+file <- args[1]  # The input file containing SNP effects
+output <- args[2]  # The output file name
 
-library(HDInterval)
-library(data.table)
-library(ggplot2)
-library(matrixStats)
-
-
+# Read the input file into a data frame
 Df <- as.data.frame(fread(file))
 print('Data frame loaded')
 
-row.names(Df) <- paste0(Df$CHROM,'_',Df$POS)
-Df$V1 <- NULL
-chrom <- Df$CHROM
-Df$CHROM <- NULL
-Df$POS <- NULL
+# Set row names of the data frame to the combination of CHROM and POS columns
+row.names(Df) <- paste0(Df$CHROM, '_', Df$POS)
+Df$V1 <- NULL  # Remove the first column if it exists
+chrom <- Df$CHROM  # Extract chromosome information
+Df$CHROM <- NULL  # Remove the CHROM column from the data frame
+Df$POS <- NULL  # Remove the POS column from the data frame
 
-HDI<-hdi(t(Df))
+# Compute the 98% highest density intervals (HDI) for the SNP effects
+HDI <- hdi(t(Df))
 print('Credibility interval computed')
 
+# Function to check if zero is within the interval
 is_zero_in_interval <- function(lower, upper) {
   return(lower <= 0 & upper >= 0)
 }
 
+# Identify credible SNPs where zero is not in the interval
 sig <- colnames(HDI)[!apply(HDI, 2, function(x) is_zero_in_interval(x["lower"], x["upper"]))]
 print('Credible SNPs found')
 
-# Ajouter des colonnes pour la médiane et les intervalles de crédibilité
+# Add columns for the median and the credibility intervals
 Df$median <- rowMedians(as.matrix(Df))
-Df$lower <- HDI['lower',]
-Df$upper <- HDI['upper',]
+Df$lower <- HDI['lower', ]
+Df$upper <- HDI['upper', ]
 
-# Ajouter une colonne pour indiquer les SNPs significatifs
+# Add a column to indicate credible SNPs
 Df$credible <- ifelse(rownames(Df) %in% sig, "Credible", "Not Credible")
 
+# Add back the chromosome information
 Df$CHROM <- as.factor(chrom)
 
-write.csv(Df[,c('CHROM','median','lower','upper','credible')],gsub('XXX',output,'Summary_XXX.csv'))
+# Write the summary to a CSV file
+write.csv(Df[, c('CHROM', 'median', 'lower', 'upper', 'credible')], gsub('XXX', output, 'Summary_XXX.csv'))
