553ceb4e1514f8ae737845bdddeeb3706a8d2cd6
Xillanne <83120878+Xillanne@users.noreply.github.com>
2024-06-28 14:26:38 +0200

    Update 3-MCMCglmm_model.R

--- Pipeline_GBLUP/Multivariate/3-MCMCglmm_model.R
+++ Pipeline_GBLUP/Multivariate/3-MCMCglmm_model.R
@@ -1,13 +1,13 @@
-## Script to developp GWAS using MCMCglmm : GBLUP step
+## Script to develop GWAS using MCMCglmm: GBLUP step
 
-
+# Get command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
-matrix <- args[1]
-M_file <- args[2] 
-condition <- args[3]
-output <- args[4]
+matrix <- args[1]  # Path to the matrix file
+M_file <- args[2]  # Path to the M file
+condition <- args[3]  # Experimental condition
+output <- args[4]  # Output directory
 
-# Load packages
+# Load necessary packages
 options(rgl.useNULL=TRUE)
 library(data.table)
 library(lattice)
@@ -17,54 +17,46 @@ library(reshape2)
 library(gridExtra)
 library(matrixStats)
 
+# Load and process the kinship matrix
 Ginv <- as.matrix(read.csv(matrix))
-colnames(Ginv) <- gsub('CeMee','',colnames(Ginv))
-colnames(Ginv) <- gsub('_sorted','',colnames(Ginv))
-rownames(Ginv)<-colnames(Ginv)
+colnames(Ginv) <- gsub('CeMee', '', colnames(Ginv))  # Clean column names
+colnames(Ginv) <- gsub('_sorted', '', colnames(Ginv))  # Clean column names
+rownames(Ginv) <- colnames(Ginv)  # Set row names to match column names
+
+# Load and process the M matrix
 M <- as.data.frame(fread(M_file))
-rownames(M) <- M[,1]
-M[,1] <- NULL
-M <- as.matrix(M)
+rownames(M) <- M[, 1]  # Set row names
+M[, 1] <- NULL  # Remove the first column
+M <- as.matrix(M)  # Convert to matrix
 
-print(paste0(output,': matrices loaded'))
-# Load pheno data
+print(paste0(output, ': matrices loaded'))
+
+# Load phenotype data
 pheno <- read.csv('Final_Transition_rates_estimates_may2024_export.csv')
-pheno <- pheno[,c('pop_label','temperature','rel_humidity',"session_id",
-                  'logD','date_str','env_label',
-                  "SF",
-                  "SB",
-                  "FS",
-                  "FB",
-                  "BS",
-                  "BF")]
+pheno <- pheno[, c('pop_label', 'temperature', 'rel_humidity', "session_id", 'logD', 'date_str', 'env_label', "SF", "SB", "FS", "FB", "BS", "BF")]
 
-## Kept only lines with phenotypes
-pheno_subset<-pheno[pheno$pop_label %in% colnames(Ginv),]
-pheno_subset<-pheno_subset[pheno_subset$env_label==condition,]
-#pheno_subset<-pheno_subset[complete.cases(pheno_subset[,c('temperature', 'rel_humidity', 'logD')]),]
-Ginv <- Ginv[rownames(Ginv) %in% pheno_subset$pop_label,rownames(Ginv) %in% pheno_subset$pop_label]
-M <- M[rownames(M) %in% pheno_subset$pop_label,]
+# Keep only lines with phenotypes
+pheno_subset <- pheno[pheno$pop_label %in% colnames(Ginv), ]
+pheno_subset <- pheno_subset[pheno_subset$env_label == condition, ]
+Ginv <- Ginv[rownames(Ginv) %in% pheno_subset$pop_label, rownames(Ginv) %in% pheno_subset$pop_label]
+M <- M[rownames(M) %in% pheno_subset$pop_label, ]
 
-
-# Scale covariates and reduce traits
-vect_P_traits <- c("SF",
-                   "SB",
-                   "FS",
-                   "FB",
-                   "BS",
-                   "BF")
-
+# Scale covariates and standardize traits
+vect_P_traits <- c("SF", "SB", "FS", "FB", "BS", "BF")
 pheno_subset$temperature <- as.numeric(pheno_subset$temperature)
 pheno_subset$rel_humidity <- as.numeric(pheno_subset$rel_humidity)
 pheno_subset$logD <- as.numeric(pheno_subset$logD)
-for(j in c('temperature',"rel_humidity","logD")){
-  pheno_subset[,j][is.na(pheno_subset[,j])] <- mean(pheno_subset[,j], na.rm = TRUE)
-  pheno_subset[,j] <- (pheno_subset[,j]-mean(pheno_subset[,j], na.rm = TRUE))/sd(pheno_subset[,j], na.rm = TRUE)
-}
-for(j in vect_P_traits){
-  pheno_subset[,j]<-(pheno_subset[,j]-mean(pheno_subset[,j], na.rm = TRUE))
+
+# Standardize covariates
+for (j in c('temperature', "rel_humidity", "logD")) {
+  pheno_subset[, j][is.na(pheno_subset[, j])] <- mean(pheno_subset[, j], na.rm = TRUE)
+  pheno_subset[, j] <- (pheno_subset[, j] - mean(pheno_subset[, j], na.rm = TRUE)) / sd(pheno_subset[, j], na.rm = TRUE)
 }
 
+# Center traits
+for (j in vect_P_traits) {
+  pheno_subset[, j] <- (pheno_subset[, j] - mean(pheno_subset[, j], na.rm = TRUE))
+}
 
 # Plot phenotype data
 plots <- lapply(c(vect_P_traits, 'temperature', 'rel_humidity', 'logD'), function(i) {
@@ -72,26 +64,27 @@ plots <- lapply(c(vect_P_traits, 'temperature', 'rel_humidity', 'logD'), functio
     geom_histogram(aes_string(x = i), fill = "grey", color = "black", bins = floor(nrow(pheno_subset) / 3)) +
     theme_bw()
 })
-pdf(gsub('XXX',output,"pheno_subset_plots_XXX.pdf"))
+pdf(gsub('XXX', output, "pheno_subset_plots_XXX.pdf"))
 grid.arrange(grobs = plots, ncol = 3)
 dev.off()
 
-
 #### GBLUP ####
-# Model as : y= Xb + Wr + u + e 
-# X, fixed effects
-# W, random effects
-# u, follow normal distribution with variance of A*Var(u)
+# Model as: y = Xb + Wr + u + e 
+# X: fixed effects
+# W: random effects
+# u: follows normal distribution with variance of A*Var(u)
 
 # Define prior model for GBLUP
 nb_trait = length(vect_P_traits)
 phen.var = diag(nb_trait) * diag(var(subset(pheno_subset, select = vect_P_traits)))
-prior_mod <- list(G = list(G1 = list(V = phen.var/3, nu = nb_trait),
-                           G2 = list(V = phen.var/3, nu = nb_trait)), 
-                  R = list(V = phen.var/3, nu = nb_trait))
+prior_mod <- list(G = list(G1 = list(V = phen.var / 3, nu = nb_trait),
+                           G2 = list(V = phen.var / 3, nu = nb_trait)), 
+                  R = list(V = phen.var / 3, nu = nb_trait))
+
+# Convert Ginv to sparse matrix format
+Ginv <- as(Ginv, "dgCMatrix")
 
 # Perform GBLUP using MCMCglmm
-Ginv <- as(Ginv, "dgCMatrix")
 if (length(unique(pheno_subset$session_id)) != 1) {
   model_MCMC_WI <- MCMCglmm(cbind(SF, SB, FS, FB, BS, BF) ~ (logD + rel_humidity + temperature + session_id) + trait - 1, 
                             random = ~us(trait):pop_label + us(trait):date_str, 
@@ -118,16 +111,9 @@ if (length(unique(pheno_subset$session_id)) != 1) {
                             pr = TRUE)
 }
 
-# # Save model results
+# Save model results
 write.csv(model_MCMC_WI$Sol, gsub('XXX', output, 'XXX_MCMCmodel_Sol.csv'), quote = FALSE, row.names = FALSE)
 write.csv(model_MCMC_WI$VCV, gsub('XXX', output, 'XXX_MCMCmodel_VCV.csv'), quote = FALSE, row.names = FALSE)
 
 # Print model summary
 print(summary(model_MCMC_WI))
-
-# # Exit with a status code
-# if (length(unique(pheno_subset$session_id)) != 1) {
-#   quit(status = 0)
-# } else {
-#   quit(status = 1)
-# }
